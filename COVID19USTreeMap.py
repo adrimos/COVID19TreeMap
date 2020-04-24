6117bfc263dfd2eaca3b2ba954f942dbdefb41bd
Adri <adriana.moscatelli@gmail.com>
2020-04-23 18:09:49 -0700

    added title and percentage of state total

--- COVID19USTreeMap.py
+++ COVID19USTreeMap.py
@@ -10,9 +10,21 @@ import plotly as pl
 
 # %%
 # Load the data from the John Hopkins github repo
-df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-20-2020.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-23-2020.csv')
 
-    # %%
+
+# %%
+# Dropping some columns and sorting
+
+df1 = df[["Admin2", "Province_State", "Country_Region", "Confirmed", "Deaths", "Combined_Key"]] #getting the columns I want
+df1 = df1[(df1["Country_Region"] == "US")] #dropping countries other than the US
+df1 = df1.rename(columns={'Province_State': 'State'})
+df1 = df1.rename(columns={'Admin2': 'County'})
+df1 = df1.rename(columns={'Country_Region': 'Country'})
+df1 = df1.rename(columns={'Combined_Key': 'County/State'})
+
+
+# %%
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
@@ -48,9 +60,20 @@ def build_hierarchical_dataframe(df, levels, value_column, color_columns=None):
     df_all_trees = df_all_trees.append(total, ignore_index=True)
     return df_all_trees
 
+
 df_all_trees = build_hierarchical_dataframe(df1, levels, value_column, color_columns)
 avg = df1['Confirmed'].mean()
 
+
+
+# %%
+# Creating a categorical column to use in the treemap to use colors for each state
+codes = pd.DataFrame(df_all_trees['parent'].astype('category'))
+codes["parent"] = codes["parent"].cat.codes
+df_all_trees['codes'] = codes['parent']
+
+
+# %%
 fig = make_subplots(1, 1, specs=[[{"type": "domain"}]])
 
 fig.add_trace(go.Treemap(
@@ -59,26 +82,30 @@ fig.add_trace(go.Treemap(
     values=df_all_trees['value'],
     branchvalues='total',
     marker=dict(
-        colors=df_all_trees['color'],
+        colors=df_all_trees['codes'],
         colorscale='matter',
-        cmid=avg),
-    hovertemplate='<b>%{label} </b> <br> Confirmed: %{value:,.2s}<br> Deaths: %{color:,.2s}<br> %{percentParent:,2f}',
-    texttemplate='<b>%{label} </b> <br> Confirmed: %{value:,.2s} <br> %{percentParent} of state total',
+        #cmid=0.5
+        ),
+    hovertemplate='<b>%{label} </b> <br> Confirmed: %{value:,.2s}<br> %{percentParent:,.0%} of state total<extra></extra>',
+    texttemplate='<b>%{label} </b> <br> Confirmed: %{value:,.2s} <br> %{percentParent} of state total<br>',
     maxdepth=3,
+    meta=df_all_trees['codes']
     ), 1, 1)
 
-fig.update_layout(margin=dict(t=0, b=0, r=0, l=0))
+fig.update_layout(
+    title='Cumulative confirmed cases per state and county as percentage of total in the US',
+    title_x=0.5,
+    hoverlabel=dict(
+            bgcolor="white",
+            font_size=16,
+            font_color="#595959",
+            font_family="Arial",
+            bordercolor='#595959'
+            ),
+    )
 fig.show()
 
 import plotly.io as pio
 pio.write_html(fig, file='Index.html', auto_open=True)
-
-# %%
-codes = pd.factorize(df1['State'])
-
-
-
-#df_all_trees['State Codes'] = codes
-
 
 # %%
