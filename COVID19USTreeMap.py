c1ab956c4f0dde7b424e89d0af4dd64e00d8fda4
Adri <adriana.moscatelli@gmail.com>
2020-07-03 18:34:22 -0700

    Data refresh

--- COVID19USTreeMap.py
+++ COVID19USTreeMap.py
@@ -10,7 +10,7 @@ import plotly as pl
 
 # %%
 # Load the data from the John Hopkins github repo
-df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/06-30-2020.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/07-02-2020.csv')
 
 
 # %%
